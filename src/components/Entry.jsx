e7002a498ad64e7c2bdcf51b49ebfbe71808b8d1
Zee Jay <zj.bulawan@gmail.com>
2025-02-13 14:29:15 +0800

    Update: Adding data.js and iterate data using map to create array of jsx element to display on the page

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,17 +1,19 @@
-export default function Entry() {
+export default function Entry(props) {
+
+  const {img, title, country, googleMapsLink, dates, description} = props;
   return (
     <article className="journal-entry__list">
       <figure className="journal-entry__img-area">
-        <img src="../src/assets/images/mt-fuji.png" alt="text here" />
+        <img src={img.src} alt={img.alt} />
       </figure>
       <div className="journal-entry__txt-area">
         <div className="map-info">
-          <span className="map-info__location">JAPAN</span>
-          <span className="map-info__link"><a href="">View on Google Maps</a></span>
+          <span className="map-info__location">{country}</span>
+          <span className="map-info__link"><a href={googleMapsLink}>View on Google Maps</a></span>
         </div>
-        <h2 className="title">Mount Fuji</h2>
-        <div className="date">12 Jan, 2023 - 24 Jan, 2023</div>
-        <div className="description">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</div>
+        <h2 className="title">{title}</h2>
+        <div className="date">{dates}</div>
+        <div className="description">{description}</div>
       </div>
     </article>
   )
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,28 @@
 import Entry from "./Entry"
+import dataEntry from "../assets/data/data.js"
+
+
+console.log(dataEntry)
+
+const dataEntryElem = dataEntry.map(entry => {
+  return (
+    <Entry
+      key={entry.id}
+      img={entry.img}
+      title={entry.title}
+      country={entry.country}
+      googleMapsLink={entry.googleMapsLink}
+      dates={entry.dates}
+      description={entry.text}
+      />
+  )
+})
 
 export default function Main() {
+
   return (
     <div className="journal-entry">
-      <Entry />
-      <Entry />
-      <Entry />
-      <Entry />
+      {dataEntryElem}
     </div>
-    
   )
 }
