7bf4de3cd02e29d33ba1f58c282be8e37947f23d
Zee Jay <zj.bulawan@gmail.com>
2025-02-13 14:33:53 +0800

    Update: Cleaning the code

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,24 +1,21 @@
 import Entry from "./Entry"
 import dataEntry from "../assets/data/data.js"
 
-
-console.log(dataEntry)
-
-const dataEntryElem = dataEntry.map(entry => {
-  return (
-    <Entry
-      key={entry.id}
-      img={entry.img}
-      title={entry.title}
-      country={entry.country}
-      googleMapsLink={entry.googleMapsLink}
-      dates={entry.dates}
-      description={entry.text}
-      />
-  )
-})
-
 export default function Main() {
+
+  const dataEntryElem = dataEntry.map(entry => {
+    return (
+      <Entry
+        key={entry.id}
+        img={entry.img}
+        title={entry.title}
+        country={entry.country}
+        googleMapsLink={entry.googleMapsLink}
+        dates={entry.dates}
+        description={entry.text}
+        />
+    )
+  })
 
   return (
     <div className="journal-entry">
